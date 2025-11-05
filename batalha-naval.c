bc91b8cbc9c87c069cafbe337fcf6522704c6984
miguelpazatto <miguelpazatto60@gmail.com>
2025-11-05 18:56:15 +0000

    Desafio nível aventureiro

--- batalha-naval.c
+++ batalha-naval.c
@@ -1,75 +1,148 @@
 #include <stdio.h>
 
+#define linhas 10
+#define colunas 10
+
 int main () {
 
-   int tabuleiro[10][10];
-   int i, j;
+    int matriz[linhas][colunas];
+      int i, j;
 
-   for (i = 0; i < 10; i++) {
-    for (j = 0; j < 10; j++) {
-        tabuleiro[i][j] = 0; 
+    for (i = 0; i < linhas; i++) {
+        for (j = 0; j < colunas; j++) {
+            matriz[i][j] = 0;
+        }
+            
+    int navioH[3] = {3, 3, 3};
+
+    int linhaH = 2, colunaH = 0;
+    int podeH;
+
+    podeH = 1;
+
+    if (!(linhaH >= 0 && linhaH < linhas && colunaH >= 0 && colunaH + 3 <= colunas)) {
+        podeH = 0;
+    }
+
+    if (podeH == 1) {
+        for (int k = 0; k < 3; k++) {
+            if (matriz[linhaH][colunaH + k] != 0) {
+                podeH = 0;
+                break;  // k é o tamanho do navio, se k achar algo diferente de 0 quebra.
+            }
+        }
+    }
+
+    if (podeH == 1) {
+        for (int k = 0; k < 3; k++) {
+            matriz[linhaH][colunaH + k] = navioH[k];
+        }
+    }
+
+
+
+    int navioV[3] = {3, 3, 3};
+
+    int linhaV = 0, colunaV = 3;
+    int podeV;
+
+    podeV = 1;
+
+    if (linhaV < 0 && linhaV + 3 > linhas && colunaV < 0 && colunaV > colunas) {
+        podeH = 0;
+    }
+   
+    if (podeH == 1) {
+        for(int k = 0; k < 3; k++) {
+            if (matriz[linhaV + k][colunaV] != 0) {
+                podeH = 0;
+                break; 
+            }
+        }
+    }
+
+    if (podeH == 1) {
+        for(int k = 0; k < 3; k++) {
+            matriz[linhaV + k][colunaV] = navioV[k];
+        }
+    }
+
+
+    int navioD1[3] = {3, 3, 3};
+
+    int linhaD1 = 4, colunaD1 = 4;
+    int podeD1;
+
+    podeD1 = 1;
+
+    if (!(linhaD1 >= 0 && linhaD1 <= 7 && colunaD1 >= 0 && colunaD1 <= 7)) {
+        podeD1 = 0;
+    }
+
+    if (podeD1 == 1) {
+        for(int k = 0; k < 3; k++) {
+            if (matriz[linhaD1 + k][colunaD1 + k] != 0) {
+                podeD1 = 0;
+                break;
+            }
+        }
+    }
+
+    if (podeD1 == 1) {
+        for(int k = 0; k < 3; k++) {
+            matriz[linhaD1 + k][colunaD1 + k] = navioD1[k];
+        }
     }
 }
 
-    int navioHorizontal[3] = {3, 3, 3};
-    int navioVertical[3] = {3, 3, 3};
 
-    int linhaH = 2, colH = 4;
-    int linhaV = 6, colV = 1;
+    int navioD2[3] = {3, 3, 3};
 
-    int podeH = 1;
+    int linhaD2 = 7, colunaD2 = 2;
+    int podeD2;
 
-    if (linhaH < 0 || linhaH >= 10) podeH = 0;
-    if (colH < 0 || colH + 3 > 10) podeH = 0;
+    podeD2 = 1;
 
-    if (podeH == 1) {
-        for (j = 0; j < 3; j++) {
-            if (tabuleiro[linhaH][colH + j] != 0) {
-                podeH = 0;
+    if (!(linhaD2 >= 0 && linhaD2 <= 7 && colunaD2 >= 2 && colunaD2 <= 7)) {
+        podeD2 = 0;
+    }
+
+    if (podeD2 == 1) {
+        for(int k = 0; k < 3; k++) {
+            if (matriz[linhaD2][9 - linhaD2] != 0) {
+                podeD2 = 0;
+                break;
             }
         }
     }
 
-    if (podeH == 1) {
-        for (j = 0; j < 3; j++) {
-            tabuleiro[linhaH][colH + j] = navioHorizontal[j];
+    if (podeD2 == 1) {
+        for(int k = 0; k < 3; k++) {
+            matriz[linhaD2 + k][colunaD2 - k] = navioD2[k];
         }
     }
 
-    int podeV = 1;
 
-    if (colV < 0 || colV >= 10) podeV = 0;
-    if (linhaV < 0 || linhaV + 3 > 10) podeV = 0;
-
-    if (podeV == 1) {
-        for (i = 0; i < 3; i++) {
-            if (tabuleiro[linhaV + i][colV] != 0) {
-                podeV = 0;
-            }
-        }
+    printf("  ");
+    for(int a = 0; a < 10; a++) {
+        printf("%d ", a);
     }
-    
-    if (podeV == 1) {
-        for (i = 0; i < 3; i++) {
-            tabuleiro[linhaV + i][colV] = navioVertical[i];
-        }
-    }
-
-    printf("    ");
-    for (j = 0; j < 10; j++) printf("%2d ", j);
     printf("\n");
 
-    printf("   ");
-    for (j = 0; j < 10; j++) printf("---");
-    printf("-\n");
+    printf("  ");
+    for(int a = 0; a < 10; a++) {
+        printf("---");
+    }
+    printf("\n");
 
-    for (i = 0; i < 10; i++) {
-        printf("%2d |", i);
-        for (j = 0; j < 10; j++) {
-            printf(" %d ", tabuleiro[i][j]);
+    for(int b = 0; b < 10; b++) {
+        printf("%d ", b);
+        for(int a = 0; a < 10; a++) {
+            printf("%d ", matriz[b][a]);
         }
         printf("\n");
-}
+
+    }
 
     return 0;
 }
